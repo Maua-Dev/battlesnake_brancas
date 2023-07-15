db78c5d972cdb1cd31ca4d45fb8a2d486568209f
João Vitor <21.01075-7@maua.br>
2023-07-15 11:09:27 -0300

    cria GET /

--- src/app/main.py
+++ src/app/main.py
@@ -3,11 +3,34 @@ from mangum import Mangum
 
 app = FastAPI()
 
-# TODO: Implement my logic here to handle the requests from Battlesnake
+"""
+GET /
+POST /start
+POST /move
+POST /end
+"""
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def info():
+    """
+    link: https://docs.battlesnake.com/api/requests/info
+    {
+        "apiversion": "1",
+        "author": "MyUsername",
+        "color": "#888888",
+        "head": "default",
+        "tail": "default",
+        "version": "0.0.1-beta"
+    }
+    """
+    return {
+        "apiversion": "1",
+        "author": "EhOBrancas",
+        "color": "#2670FF",
+        "head": "gamer",
+        "tail": "rbc-necktie",
+        "version": "1.4.4"
+    }
 
 
 @app.get("/items/{item_id}")
